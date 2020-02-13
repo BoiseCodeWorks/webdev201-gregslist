5193309868408abcd3a8e1e16738fe25b3a17c78
Jake Overall <joverall22@gmail.com>
2020-02-12 21:04:32 -0700

    adds edit

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -29,18 +29,62 @@ export default class CarsController {
       event.preventDefault();
       let form = event.target;
       let carData = {
+        // @ts-ignore
         make: form.Make.value,
+        // @ts-ignore
         model: form.Model.value,
+        // @ts-ignore
         year: form.Year.value,
+        // @ts-ignore
         price: form.Price.value,
+        // @ts-ignore
         description: form.Description.value,
+        // @ts-ignore
         imgUrl: form.ImageURL.value
       };
-      await CarsService.createCar(carData);
+
+      // @ts-ignore
+      let id = form._id.value;
+      if (id) {
+        carData._id = id;
+        await CarsService.updateCar(carData);
+      } else {
+        await CarsService.createCar(carData);
+      }
+      // @ts-ignore
+      form.reset();
     } catch (error) {
       console.log(error);
     }
   }
+  async editCar(id) {
+    let car = store.State.cars.find(c => c._id == id);
+    let form = document.getElementById("car-form");
+    // @ts-ignore
+    form.Make.value = car.make;
+    // @ts-ignore
+    form.Model.value = car.model;
+    // @ts-ignore
+    form.Year.value = car.year;
+    // @ts-ignore
+    form.Price.value = car.price;
+    // @ts-ignore
+    form.Description.value = car.description;
+    // @ts-ignore
+    form.ImageURL.value = car.imgUrl;
+    // @ts-ignore
+    form._id.value = car._id;
+  }
+
+  async updateCar() {
+    try {
+      // @ts-ignore
+      await CarsService.editCar();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async deleteCar(id) {
     try {
       await CarsService.deleteCar(id);
--- app/Models/Car.js
+++ app/Models/Car.js
@@ -1,17 +1,16 @@
 export default class Car {
-    constructor(data) {
-        this._id = data._id;
-        this.make = data.make;
-        this.model = data.model;
-        this.year = data.year;
-        this.description = data.description;
-        this.price = data.price;
-        this.imgUrl = data.imgUrl;
-    }
+  constructor(data) {
+    this._id = data._id;
+    this.make = data.make;
+    this.model = data.model;
+    this.year = data.year;
+    this.description = data.description;
+    this.price = data.price;
+    this.imgUrl = data.imgUrl;
+  }
 
-    get Template() {
-
-     return /* html */ `
+  get Template() {
+    return /* html */ `
      <div class="col-12 col-md-4 col-lg-3">
         <div class="card">
             <img src="${this.imgUrl}" class="card-img-top" alt="a car image">
@@ -20,9 +19,10 @@ export default class Car {
                 <div class="card-subtitle">${this.price}</div>
                 <p class="card-text">${this.description}</p>
             </div>
+            <button class="btn btn-info" onclick="app.carsController.editCar('${this._id}')">Edit</button>
             <button class="btn btn-danger" onclick="app.carsController.deleteCar('${this._id}')">Delete</button>
         </div>
      </div>
         `;
-    }
+  }
 }
